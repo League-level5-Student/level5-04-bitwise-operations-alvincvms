8248602339a144b3e86c4f500b885457da2c30a9
alvincvms <48074117+alvincvms@users.noreply.github.com>
2020-06-18 15:31:27 -0700

    starting mod 4

--- src/_01_Bit_Shifting/BitShifter.java
+++ src/_01_Bit_Shifting/BitShifter.java
@@ -1,5 +1,7 @@
 package _01_Bit_Shifting;
 
+import _00_Binary_Conversion.DecimalToBinary;
+
 public class BitShifter {
 	public static void main(String[] args) {
 		// 1. Jot down the value of num in binary.
@@ -7,11 +9,13 @@ public class BitShifter {
 		int numShifted = num << 1;
 		
 		// 2. Print the value of numShifted, and convert that value to binary.
-		
+		System.out.println(numShifted);
+		numShifted = Integer.parseInt(DecimalToBinary.convertDecimalToBinary(numShifted));
+		System.out.println(numShifted);
 		// 3. Compare the two binary values. Can you figure out what the << operator is for?
 		
 		// 4. Try shifting num 3 places.
-		
+		System.out.println(num << 3);
 		// FYI: Binary values can be shifted to the right as well using the >> operator.	
 	}
 }
